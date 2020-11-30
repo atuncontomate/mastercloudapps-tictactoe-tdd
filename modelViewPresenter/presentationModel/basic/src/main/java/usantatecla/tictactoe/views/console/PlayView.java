eace4a16949aed2b3efc23e6293ecc7770c3bd85
Natalia <user@iMac-de-user.local>
2020-11-30 23:19:39 +0100

    [TDD-04] Refactor again

--- modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/tictactoe/views/console/PlayView.java
+++ modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/tictactoe/views/console/PlayView.java
@@ -47,26 +47,6 @@ class PlayView {
         } while (!error.isNull());
     }
 
-    private void move() {
-        boolean isUser = this.playController.isUser();
-        Coordinate origin;
-        Coordinate target;
-        Error error;
-        do {
-            if (isUser) {
-                origin = new CoordinateView().read(Message.COORDINATE_TO_REMOVE.toString());
-                target = new CoordinateView().read(Message.COORDINATE_TO_MOVE.toString());
-            } else {
-                origin = createRandomCoordinate();
-                target = createRandomCoordinate();
-            }
-            error = this.playController.move(origin, target);
-            if (isUser) {
-                new ErrorView(error).writeln();
-            }
-        } while (!error.isNull());
-    }
-
     private void move(PlayController playController) {
         boolean isUser = playController.isUser();
         Coordinate origin;
