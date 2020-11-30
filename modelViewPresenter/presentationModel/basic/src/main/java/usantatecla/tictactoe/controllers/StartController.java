4705aba0203a1df1245cf69a68dee3a6958e9c80
Natalia <user@iMac-de-user.local>
2020-12-01 00:59:35 +0100

    [TDD-13] Green

--- modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/tictactoe/controllers/StartController.java
+++ modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/tictactoe/controllers/StartController.java
@@ -1,15 +1,24 @@
 package usantatecla.tictactoe.controllers;
 
 import usantatecla.tictactoe.models.Game;
+import usantatecla.tictactoe.models.State;
 
 public class StartController extends Controller {
+
+  private State state;
 
   public StartController(Game game) {
     super(game);
   }
 
+  public StartController(Game game, State state) {
+    super(game);
+    this.state = state;
+  }
+
   public void setUsers(int users) {
     this.game.setUsers(users);
+    this.state.next();
   }
 
   public int getMaxPlayers() {
