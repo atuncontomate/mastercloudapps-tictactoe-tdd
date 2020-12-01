8fdcaccf24ececa2e7243e81013a2ecfdcbf57b3
Natalia <user@iMac-de-user.local>
2020-12-01 01:09:00 +0100

    [TDD-14] Green

--- modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/tictactoe/controllers/ResumeController.java
+++ modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/tictactoe/controllers/ResumeController.java
@@ -1,15 +1,24 @@
 package usantatecla.tictactoe.controllers;
 
 import usantatecla.tictactoe.models.Game;
+import usantatecla.tictactoe.models.State;
 
 public class ResumeController extends Controller {
+
+  private State state;
 
   public ResumeController(Game game) {
     super(game);
   }
 
+  public ResumeController(Game game, State state) {
+    super(game);
+    this.state = state;
+  }
+
   public void resume() {
     this.game.reset();
+    this.state.reset();
   }
 
 }
